903ef3284827a0bc8c06453b235206a87050c28a
ikuma0366 <103302989+ikuma0366@users.noreply.github.com>
2023-12-19 13:59:03 +0900

    Create main.js

---
+++ docs/main.js
@@ -0,0 +1,18 @@
+// XMLHttpRequestを使ってjsonデータを読み込む
+let requestURL = 'datas/kanji.jsonc';//jsonへのパス
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+ 
+// JSONデータをJavaScriptオブジェクトに変換
+request.onload = function () {
+  let data = request.response;
+  data = JSON.parse(JSON.stringify(data));
+  dataArray(data);
+}
+ 
+// foreachでJSONデータをHTMLに出力
+function dataArray(els) {
+  console.log(els);
+}
